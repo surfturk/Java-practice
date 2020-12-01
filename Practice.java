32e3612671b2734c97d50ef378fa756782e25b2d
surfturk <berkleydeane@gmail.com>
2020-12-01 12:17:04 -0600

    boolean and char sets

--- Practice.java
+++ Practice.java
@@ -4,6 +4,8 @@ public class Practice {
         byte age = 34;
         long viewsCount = 3_123_456_789L;
         float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
         System.out.println(viewsCount);
     }
 }
