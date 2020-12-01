607f51d45fbe54faa21fb1bec14145cef60ddd9c
surfturk <berkleydeane@gmail.com>
2020-12-01 12:23:10 -0600

    reference distinction

--- Practice.java
+++ Practice.java
@@ -1,11 +1,12 @@
+
+
+import java.util.Date;
+
 public class Practice {
 
     public static void main(String[] args) {
         byte age = 34;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = true;
-        System.out.println(viewsCount);
+        Date now = new Date();
+        System.out.println(now);
     }
 }
