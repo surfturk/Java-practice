2280182e0023acd91fc288553e04edf2491ee0d1
surfturk <berkleydeane@gmail.com>
2020-12-01 12:16:32 -0600

    float and double vars

--- Practice.java
+++ Practice.java
@@ -2,7 +2,8 @@ public class Practice {
 
     public static void main(String[] args) {
         byte age = 34;
-        int viewsCount = 123_456_789;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
         System.out.println(viewsCount);
     }
 }
