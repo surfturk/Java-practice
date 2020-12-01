e43b6052886e3573639588f8d0c1ed5563801eac
surfturk <berkleydeane@gmail.com>
2020-12-01 12:31:28 -0600

    reference type practice

--- Practice.java
+++ Practice.java
@@ -1,5 +1,6 @@
 
 
+import java.awt.*;
 import java.util.Date;
 
 public class Practice {
@@ -7,6 +8,12 @@ public class Practice {
     public static void main(String[] args) {
         byte age = 34;
         Date now = new Date();
-        System.out.println(now);
+
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
+
     }
+
 }
