13a6442c34ff77aa1d666013fdcbb6cd5f6473c0
surfturk <berkleydeane@gmail.com>
2020-12-10 13:19:05 -0600

    control flow, if statements

--- ControlFlow.java
+++ ControlFlow.java
@@ -7,14 +7,28 @@ public class ControlFlow {
         int y = 1;
         System.out.println(x != y);
 
+        // intro
         int temperature = 22;
         boolean isWarm = temperature > 20 && temperature < 30;
         System.out.println(isWarm);
 
+        // booleans
         boolean hasHighIncome = true;
         boolean hasGoodCredit = true;
         boolean hasCriminalRecord = false;
         boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+
+        // if statements
+        int temp = 86;
+        if (temp > 85) {
+            System.out.println("It's a hot day!");
+            System.out.println("Drink water!");
+        }
+        else if (temp > 70)
+            System.out.println("Beautiful day!");
+        else
+            System.out.println("Cold day!");
+
 
     }
 }
