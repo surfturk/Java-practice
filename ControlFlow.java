8eab964ba4a68b51f25a1c5d8595a01834c28843
surfturk <berkleydeane@gmail.com>
2020-12-10 10:54:34 -0600

    control flow ext 1

--- ControlFlow.java
+++ ControlFlow.java
@@ -6,5 +6,9 @@ public class ControlFlow {
         int x = 1;
         int y = 1;
         System.out.println(x != y);
+
+        int temperature = 22;
+        boolean isWarm = temperature > 20 && temperature < 30;
+        System.out.println(isWarm);
     }
 }
