b2b685671e5534d2ba75ea498b263c5daf9dfb90
surfturk <berkleydeane@gmail.com>
2020-12-14 12:51:43 -0600

    starting with methods

---
+++ CleanCode.java
@@ -0,0 +1,11 @@
+public class CleanCode {
+
+    public static void main(String[] args) {
+        greetUser("Berkley", "Brown");
+
+    }
+
+    public static void greetUser(String firstName, String lastName) {
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+}
