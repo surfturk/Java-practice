ff35c7a7fea5ea425645d4efb83c22b7807921aa
surfturk <berkleydeane@gmail.com>
2020-12-16 12:35:50 -0600

    intro to procedural programming

--- main.java
+++ main.java
@@ -2,13 +2,25 @@ public class main {
 
     public static void main(String[] args) {
         // var is static in java, can't change
-        var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text);
+//        var textBox1 = new TextBox();
+//        textBox1.setText("Box 1");
+//        System.out.println(textBox1.text);
+//
+//        var textBox2 = new TextBox();
+//        textBox2.setText("Box 2");
+//        System.out.println(textBox2.text);
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
+        int baseSalary = 50_000;
+        int extraHours = 10;
+        int hourlyRate = 20;
 
+    }
+
+    public static int calculateWage (
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ) {
+        return baseSalary + extraHours * hourlyRate;
     }
 }
