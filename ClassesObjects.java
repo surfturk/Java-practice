304b3ae2195e305744ae83a36cbbe897e163a03b
surfturk <berkleydeane@gmail.com>
2020-12-16 12:17:08 -0600

    TextBox class

--- ClassesObjects.java
+++ ClassesObjects.java
@@ -2,5 +2,13 @@ public class ClassesObjects {
     public class TextBox {
       public String text; // field
 
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public void  clear() {
+            this.text = "";
+        }
+
     }
 }
