8253195ad87d4b693b099fd2e22505be109a01b0
surfturk <berkleydeane@gmail.com>
2021-01-02 01:04:12 -0600

    added private methods

--- Employee.java
+++ Employee.java
@@ -1,6 +1,6 @@
 public class Employee {
  private int baseSalary;
- public int hourlyRate;
+ private int hourlyRate;
 
  public int calculateWage(int extraHours) {
      return baseSalary + (hourlyRate * extraHours);
@@ -11,6 +11,24 @@ public class Employee {
          throw new IllegalArgumentException("Salary can not be 0 or less.");
          this.baseSalary = baseSalary;
  }
+
+ public int getBaseSalary() {
+     return baseSalary;
+ }
+
+ public void setHourlyRate(int hourlyRate) {
+     if (hourlyRate <= 0)
+     throw new IllegalArgumentException("Hourly rate can not be 0 or less.");
+     this.hourlyRate = hourlyRate;
+
+ }
+
+ public int getHourlyRate() {
+     return hourlyRate;
+ }
+
+
+
 
 
 
--- main.java
+++ main.java
@@ -3,7 +3,9 @@ public class main {
     public static void main(String[] args) {
         var employee = new Employee();
         employee.setBaseSalary(-1);
-        employee.hourlyRate = 20;
+        employee.getBaseSalary();
+        employee.setHourlyRate(20);
+        employee.getHourlyRate();
         int wage = employee.calculateWage(10);
 
         System.out.println(wage);
