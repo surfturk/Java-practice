637aab825a04348d383468281b2d3c1730fb0cc7
surfturk <berkleydeane@gmail.com>
2021-01-02 01:18:45 -0600

    added private methods

--- main.java
+++ main.java
@@ -2,12 +2,11 @@ public class main {
 
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.setBaseSalary(-1);
+        employee.setBaseSalary(50_000);
         employee.getBaseSalary();
         employee.setHourlyRate(20);
-        employee.getHourlyRate();
+        employee.getBaseSalary();
         int wage = employee.calculateWage(10);
-
         System.out.println(wage);
 
     }
